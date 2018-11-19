8d848cb4f279bcc15d3164f96fedc4dc0fdf116d
sklechak1 <35785166+sklechak1@users.noreply.github.com>
2018-11-19 06:50:31 -0600

    Updating
    Changes.  Added additional contact info.

--- app/src/main/java/com/example/klechak/hamradiotools2/Logger.java
+++ app/src/main/java/com/example/klechak/hamradiotools2/Logger.java
@@ -2,6 +2,9 @@ package com.example.klechak.hamradiotools2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.text.DateFormat;
@@ -10,6 +13,10 @@ import java.util.Calendar;
 public class Logger extends AppCompatActivity {
     private TextView textViewlogger;
     private TextView textViewDate;
+    private TextView txtPower;
+    private TextView txtFreq;
+    private TextView txtComments;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +34,75 @@ public class Logger extends AppCompatActivity {
 
         textViewDate = findViewById(R.id.txtViewDisplayDate);
         textViewDate.setText(curDate);
+        String logDate = curDate;
+
+
+        txtPower = findViewById(R.id.txtPower);
+        String stringPower = txtPower.getText().toString();
+
+        txtFreq = findViewById(R.id.txtFreq);
+        String stringFreq = txtFreq.getText().toString();
+
+        txtComments = findViewById(R.id.txtComments);
+        String stringComments = txtComments.getText().toString();
+
+
+        Button btnSubmitLog = findViewById(R.id.btnSubmitLog);
+
+
+        btnSubmitLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LogContact();
+
+
+            }
+        });
     }
+
+    private void LogContact() {
+        final RadioGroup radGpMode = (RadioGroup) findViewById(R.id.rgMode);
+
+
+        Bundle bundle = getIntent().getExtras();
+        String callsign = bundle.getString("callsign");
+
+        Calendar calendar = Calendar.getInstance();
+        String curDate = DateFormat.getDateTimeInstance().format(calendar.getTime());
+        String stringFreq = txtFreq.getText().toString();
+        String stringPower = txtPower.getText().toString();
+        String stringComments = txtComments.getText().toString();
+        String stringMode;
+
+        int selectedMode = radGpMode.getCheckedRadioButtonId();
+            if (selectedMode == 1){
+                stringMode = "USB";
+            }else if (selectedMode == 2){
+                stringMode = "LSB";
+            }else if (selectedMode == 3) {
+                stringMode = "AM";
+            }else if (selectedMode == 4) {
+                stringMode = "FM";
+            }else{
+                stringMode = "Unknown Mode";
+            }
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
 }
