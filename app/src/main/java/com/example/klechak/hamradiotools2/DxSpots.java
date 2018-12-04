cff33ab212e700e8ec1a9c6590c5b2c55e4b34fa
Sean Klechak <sklechak@students.mchenry.edu>
2018-12-04 16:39:27 -0600

    Done-ski
    Ill call this good.

--- app/src/main/java/com/example/klechak/hamradiotools2/DxSpots.java
+++ app/src/main/java/com/example/klechak/hamradiotools2/DxSpots.java
@@ -10,28 +10,35 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class DxSpots extends AppCompatActivity {
+    private TextView info;
+//    TextView info = (TextView) findViewById(R.id.txt1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//         info = (TextView)findViewById(R.id.txt1);
+        setContentView(R.layout.activity_dx_spots);
 //        TextView info = (TextView) findViewById(R.id.txt1);
+         info = (TextView)findViewById(R.id.txt2);
         new parse().execute();
     }
 
-    public class parse extends AsyncTask<Void, Void, Void> {
+    public class parse extends AsyncTask<String, Void, String> {
         String dataget;
         String datarows;
         String ss;
+        private static final String FILE_NAME = "dxspots.txt";
+        FileOutputStream callFO = null;
+        StringBuffer buffer = new StringBuffer();
+
 
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected String doInBackground(String... strings) {
 //
-            TextView info = (TextView) findViewById(R.id.txt1);
+//            TextView info = (TextView) findViewById(R.id.txt1);
             try {
                 Document doc = Jsoup.connect("https://www.qrz.com/dxcluster").get();
                 Elements tables = doc.select("table");
@@ -48,16 +55,21 @@ public class DxSpots extends AppCompatActivity {
                         for (Element column : columns) {
 
                             output.append(column.text());
+                            // THIS IS THE KEY .. append colum text to string.
 //                            System.out.println();
                             System.out.print(column.text());
-////                            ss = columns.text();
+//                            ss = output.toString();
+                            buffer.append(output + "\n\n");
+
 
 
 
                         }
                         System.out.println();
 //                        info.setText(output.toString());
-//                        info.setText("Hello");
+//                          info.setText(ss);
+//                        FileOutputStream callFO = null;
+
 
 
                     }
@@ -71,13 +83,14 @@ public class DxSpots extends AppCompatActivity {
             }
 
 
-            return null;
+            return buffer.toString();
+
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            info.setText(s);
 
 
         }
